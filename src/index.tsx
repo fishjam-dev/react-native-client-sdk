bd536b60910c82a1975e29c84845737039b854a3
Karol Kąkol <kakol.karol00@gmail.com>
2023-11-21 18:02:51 +0100

    fix husky

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import {
   Endpoint,
+  Metadata,
   updateEndpointMetadata,
   useEndpoints,
 } from '@jellyfish-dev/react-native-membrane-webrtc';
@@ -19,13 +20,17 @@ export {
   VideoPreviewView,
 } from '@jellyfish-dev/react-native-membrane-webrtc';
 
-type Peer = Endpoint;
+type Peer<
+  Q extends Metadata,
+  T extends Metadata,
+  P extends Metadata
+> = Endpoint<Q, T, P>;
 
 /**
  * This hook provides live updates of room peers.
  * @returns An array of room peers.
  */
-export const usePeers: () => Peer[] = useEndpoints;
+export const usePeers: () => Peer<any, any, any>[] = useEndpoints;
 
 /**
  * Function that updates peer's metadata on the server.
