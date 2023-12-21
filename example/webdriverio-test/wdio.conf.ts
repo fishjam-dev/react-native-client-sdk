ab64afd0b2c1f04cf690c6454fe3abf37126ee5e
Karol Kąkol <kakol.karol00@gmail.com>
2023-12-21 10:47:49 +0100

    conf3

--- example/webdriverio-test/wdio.conf.ts
+++ example/webdriverio-test/wdio.conf.ts
@@ -20,6 +20,7 @@ export const config: Options.Testrunner = {
   maxInstances: 10,
   capabilities: capabilities,
   logLevel: 'info',
+  bail: 1,
   baseUrl: '127.0.0.1',
   waitforTimeout: 10000,
   connectionRetryTimeout: 120000,
@@ -44,6 +45,5 @@ export const config: Options.Testrunner = {
   mochaOpts: {
     ui: 'bdd',
     timeout: 60000,
-    bail: true,
   },
 };
