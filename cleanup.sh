6f405ef655c07ee817961a8138145fddd22867da
Karol Kąkol <kakol.karol00@gmail.com>
2024-03-19 20:00:17 +0100

    usefuul scripts

---
+++ cleanup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Start the tasks in the background
+(rm -rf node_modules) &
+(rm yarn.lock) &
+(cd example && rm -rf node_modules) &
+(cd example && rm yarn.lock) &
+(cd example/ios && rm -rf Pods && pod deintegrate) &
+(cd example/ios && rm -rf build) &
+(cd example/ios && rm Podfile.lock) &
+
+# Wait for all background tasks to finish
+wait
+
+echo "All tasks complete!"
