070af2b13f138864f045e1a9d06d4c58f602ab63
Miron Pawlik <miron.pawlik@swmansion.com>
2024-04-29 19:42:20 +0200

    Move example

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
+import { Metadata, Endpoint } from './MembraneWebRTC.types';
+
+export * from './MembraneWebRTC.types';
 export { useAudioSettings } from './hooks/useAudioSettings';
 export { useBandwidthEstimation } from './hooks/useBandwidthEstimation';
 export { useCamera } from './hooks/useCamera';
-export { useEndpoints } from './hooks/useEndpoints';
+export { useEndpoints as usePeers } from './hooks/useEndpoints';
 export { useMicrophone } from './hooks/useMicrophone';
 export { useRTCStatistics } from './hooks/useRTCStatistics';
 export { useScreencast } from './hooks/useScreencast';
@@ -9,7 +12,7 @@ export { useWebRTC } from './hooks/useWebRTC';
 
 export {
   updateAudioTrackMetadata,
-  updateEndpointMetadata,
+  updateEndpointMetadata as updatePeerMetadata,
   updateVideoTrackMetadata,
 } from './common/metadata';
 export {
@@ -20,4 +23,27 @@ export {
 
 export { default as VideoPreviewView } from './VideoPreviewView';
 export { default as VideoRendererView } from './VideoRendererView';
-export * from './MembraneWebRTC.types';
+export * from './JellyfishClientContext';
+
+export type Peer<
+  MetadataType extends Metadata,
+  VideoTrackMetadataType extends Metadata,
+  AudioTrackMetadataType extends Metadata,
+> = Endpoint<MetadataType, VideoTrackMetadataType, AudioTrackMetadataType>;
+
+// /**
+//  * This hook provides live updates of room peers.
+//  * @returns An array of room peers.
+//  */
+// export const usePeers: <
+//   MetadataType extends Types.Metadata,
+//   VideoTrackMetadataType extends Types.Metadata,
+//   AudioTrackMetadataType extends Types.Metadata,
+// >() => Peer<MetadataType, VideoTrackMetadataType, AudioTrackMetadataType>[] =
+//   useEndpoints;
+
+// /**
+//  * Function that updates peer's metadata on the server.
+//  * @param metadata a map `string -> any` containing user's metadata to be sent to the server.
+//  */
+// export const updatePeerMetadata = updateEndpointMetadata;
