c17e6c87a0a3ab086b35d53fb86fc69b97cd7116
Miron Pawlik <miron.pawlik@swmansion.com>
2024-05-09 09:41:38 +0200

    Add JellyfishClientContext

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
+import { Endpoint, Metadata } from './MembraneWebRTC.types';
+import { updateEndpointMetadata } from './common/metadata';
+import { useEndpoints } from './hooks/useEndpoints';
+
 export { useAudioSettings } from './hooks/useAudioSettings';
 export { useBandwidthEstimation } from './hooks/useBandwidthEstimation';
 export { useCamera } from './hooks/useCamera';
@@ -21,3 +25,28 @@ export {
 export { default as VideoPreviewView } from './VideoPreviewView';
 export { default as VideoRendererView } from './VideoRendererView';
 export * from './MembraneWebRTC.types';
+export * from './JellyfishClientContext';
+
+// below are aliases used by 'old' rn-client-sdk. They should be removed
+export type Peer<
+  MetadataType extends Metadata,
+  VideoTrackMetadataType extends Metadata,
+  AudioTrackMetadataType extends Metadata,
+> = Endpoint<MetadataType, VideoTrackMetadataType, AudioTrackMetadataType>;
+
+/**
+ * This hook provides live updates of room peers.
+ * @returns An array of room peers.
+ */
+export const usePeers: <
+  MetadataType extends Metadata,
+  VideoTrackMetadataType extends Metadata,
+  AudioTrackMetadataType extends Metadata,
+>() => Peer<MetadataType, VideoTrackMetadataType, AudioTrackMetadataType>[] =
+  useEndpoints;
+
+/**
+ * Function that updates peer's metadata on the server.
+ * @param metadata a map `string -> any` containing user's metadata to be sent to the server.
+ */
+export const updatePeerMetadata = updateEndpointMetadata;
